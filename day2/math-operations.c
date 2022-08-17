32dc19ec66da4f4b019a0e7959d7834eb06f6548
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-17 18:31:09 -0300

    chore: mathematical operations

---
+++ day2/math-operations.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("\nAddition: %d\n", 10 + 20);
+    printf("\nSubtraction: %d\n", 10 - 20);
+    printf("\nMultiplication: %d\n", 10 * 20);
+    printf("\nDivision: %.2f\n\n", 10.0 / 20);
+
+    return 0;
+}
