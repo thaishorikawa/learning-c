35dd079decc6a44ea1dafe1eee5e3acf8d4f7c48
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-17 18:51:00 -0300

    chore: findind the remainder division

---
+++ day2/remainder-division.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+
+    a = 20;
+    b = 7;
+    printf("The remainder of the division is: %d\n", a % b);
+
+    return 0;
+}
