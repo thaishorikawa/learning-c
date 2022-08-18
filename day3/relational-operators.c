3156b7925335620cb9764998eb2757a6c0282d2c
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-18 19:05:18 -0300

    chore: relational operators

---
+++ day3/relational-operators.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 10;
+    int b = 20;
+
+    printf("\n\tResult: %d\n", a != b);
+    printf("\n\tResult: %d\n", a >= b);
+    printf("\n\tResult: %d\n", a <= b);
+    printf("\n\tResult: %d\n\n", a == b);
+    return 0;
+}
