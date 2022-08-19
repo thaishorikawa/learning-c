33b7f8da7706a188226b3caa3caf9a94ac63ec27
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-19 20:06:50 -0300

    fix: identation

--- day4/logical-and-operator.c
+++ day4/logical-and-operator.c
@@ -9,9 +9,9 @@ int main()
     printf("\nWrite your gender f or m and your age: ");
     scanf("%c%d", &gender, &age);
 
-    if(gender == 'm' && age == 18)
+    if (gender == 'm' && age == 18)
         printf("\nMandatory military conscription!\n\n");
-    else 
+    else
         printf("\nDismissed!\n\n");
 
     return 0;
