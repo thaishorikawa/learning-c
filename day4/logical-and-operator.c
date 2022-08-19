04c1d5da90b802bacd3b5ddbe41053321f0a294c
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-19 20:05:58 -0300

    chore: logical AND operator

---
+++ day4/logical-and-operator.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char gender;
+    int age;
+
+    printf("\nWrite your gender f or m and your age: ");
+    scanf("%c%d", &gender, &age);
+
+    if(gender == 'm' && age == 18)
+        printf("\nMandatory military conscription!\n\n");
+    else 
+        printf("\nDismissed!\n\n");
+
+    return 0;
+}
