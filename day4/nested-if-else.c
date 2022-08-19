fcc7eae7b1c03a0e344416a2222f2c3686c9c719
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-19 18:11:30 -0300

    chore: nested if else statement

---
+++ day4/nested-if-else.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main()
+{
+    int a;
+
+    printf("\nChoose a number you want: ");
+    scanf("%d", &a);
+
+    printf("Ternary operator: \n");
+    a < 0 ? printf("\n\tNegative number!\n") : printf("\n\tPositive number or zero!\n");
+
+    printf("\n\nIf/Else:\n");
+    if (a < 0)
+        printf("\n\tNegative number!\n");
+    else
+    {
+        if (a < 0)
+            printf("\n\tPositive number!");
+        else
+            printf("\n\tNumber is zero!");
+    }
+    printf("\nLet's make the program better...\n\n");
+
+    return 0;
+}
