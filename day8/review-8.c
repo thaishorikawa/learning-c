418e530f6a074d523aa41fcd2f97a2427f8fe73e
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-23 09:56:19 -0300

    docs: remove 'with' and add 'has

--- day8/review-8.c
+++ day8/review-8.c
@@ -11,40 +11,40 @@ int main()
     switch (month)
     {
     case 1:
-        printf("\nJanuary with 31 days\n\n");
+        printf("\nJanuary has 31 days\n\n");
         break;
     case 2:
-        printf("\nFebruary with 28 days\n\n");
+        printf("\nFebruary has 28 days\n\n");
         break;
     case 3:
-        printf("\nMarch with 31 days\n\n");
+        printf("\nMarch with has days\n\n");
         break;
     case 4:
-        printf("\nApril with 30 days\n\n");
+        printf("\nApril with has days\n\n");
         break;
     case 5:
-        printf("\nMay with 31 days\n\n");
+        printf("\nMay with has days\n\n");
         break;
     case 6:
-        printf("\nJune with 30 days\n\n");
+        printf("\nJune with has days\n\n");
         break;
     case 7:
-        printf("\nJuly with 31 days\n\n");
+        printf("\nJuly with has days\n\n");
         break;
     case 8:
-        printf("\nAugust with 31 days\n\n");
+        printf("\nAugust with has days\n\n");
         break;
     case 9:
-        printf("\nSeptember with 30 days\n\n");
+        printf("\nSeptember with has days\n\n");
         break;
     case 10:
-        printf("\nOctober with 31 days\n\n");
+        printf("\nOctober with has days\n\n");
         break;
     case 11:
-        printf("\nNovember with 30 days\n\n");
+        printf("\nNovember with has days\n\n");
         break;
     case 12:
-        printf("\nDecember with 31 days\n\n");
+        printf("\nDecember with has days\n\n");
         break;
     default:
         printf("\nInvalid number!\n\n");
