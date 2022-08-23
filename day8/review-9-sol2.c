58c628cc98d7a46603615e02da5606cac7d9de30
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-23 16:51:52 -0300

    chore: review 9 - solution 2 - of exercises from day 3 to day 5

---
+++ day8/review-9-sol2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    float grade1, grade2, grade3;
+    char choice;
+
+    printf("\nDo you want to use an arithmetic or weighted average?\n\na - arithmetic\np - weighted\n\n");
+    scanf("%c", &choice);
+
+    if (choice == 'a' || choice == 'p')
+    {
+        printf("\nEnter three grades: ");
+        scanf("%f%f%f", &grade1, &grade2, &grade3);
+    }
+    if (choice == 'a')
+        printf("\nArithmetic average: %.2f\n\n", (grade1 + grade2 + grade3) / 3);
+    else if (choice == 'p')
+        printf("\nWeighted average: %.2f\n\n", (grade1 * 3 + grade2 * 3 + grade3 * 4) / 10);
+    else
+        printf("\nInvalid option!\n\n");
+
+    return 0;
+}
