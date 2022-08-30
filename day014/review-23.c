c0e18dd04efb539615f7adc460241bb52caf4e96
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-30 21:56:06 +0100

    chore: review 23 of exercises of day 8

---
+++ day014/review-23.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 1; i <= 9; i += 2)
+    {
+        for (j = 6 + i; j >= 4 + i; j--)
+            printf("I = %d\tJ = %d\n", i, j);
+        printf("\n");
+    }
+}
