10d21a9eecfa8724cf05ac28b4d6aff1c33cd5fb
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-30 17:25:49 +0100

    chore: update logical AND operator &&

--- day014/review-18.c
+++ day014/review-18.c
@@ -9,18 +9,18 @@ int main()
   {
     printf("Enter x and y: ");
     scanf("%d%d", &x, &y);
-    if (x != 0 & y != 0)
+    if (x != 0 && y != 0)
     {
-        if (x > 0 & y > 0)
+        if (x > 0 && y > 0)
             printf("Quadrant I\n");
-        else if (x < 0 & y > 0)
+        else if (x < 0 && y > 0)
             printf("Quadrant II\n");
-        else if (x < 0 & y < 0)
+        else if (x < 0 && y < 0)
             printf("Quadrant III\n");
         else
             printf("Quadrant IV\n");
     }
-  } while (x != 0 & y != 0);
+  } while (x != 0 && y != 0);
 
   return 0;    
 }
