4d6b2423a67ce1f35295c09e17a12dea8f0ed8b7
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-30 21:38:07 +0100

    chore: review 22 of exercises of day 8

---
+++ day014/review-22.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (j = 1; j <= 8; j++)
+    {
+        for (i = 1; i <= j; i++)
+            printf("*");
+        printf("\n");
+    }
+    return 0;    
+}
