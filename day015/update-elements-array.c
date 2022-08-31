b1d6e3b29d864b79eb36e8849f0ce6cf4c1539c8
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-08-31 17:10:39 +0100

    chore: updating elements in an array

---
+++ day015/update-elements-array.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    int number[10];
+
+    for (i = 0; i < 10; i++)
+    {
+        printf("Enter the number of the position %d: ", i);
+        scanf("%d", &number[i]);
+    }
+
+    for (i = 0; i < 10; i++)
+        number[i] = number[i] * 3;
+
+    printf("\n");
+    for (i = 0; i < 10; i++)
+        printf("%d ", number[i]);
+    printf("\n\n");
+
+    return 0;
+}
