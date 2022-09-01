1b0f7b236457cd8215445847df292e27c7e6fc1a
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-09-01 20:32:15 +0100

    chore: addition of two  matrices

---
+++ day016/addition-two-matrices.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int i, j, mat1[3][3] = {1, 2, 3, 4, 5, 6, 7, 8, 9}, mat2[3][3] = {10, 11, 12, 13, 14, 15, 16, 17, 18}, mat3[3][3];
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+            mat3[i][j] = mat1[i][j] + mat2[i][j];
+    }
+
+    for(i = 0; i < 3; i++)
+    {
+        for(j = 0; j < 3; j++)
+            printf("%2d ", mat3[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
