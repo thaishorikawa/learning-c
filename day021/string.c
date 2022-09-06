b4f1ff0406aa5f926e61aa064f9197b26360aa0b
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-09-06 21:22:54 +0100

    chore: string

---
+++ day021/string.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char words[] = {"Hey! Let's get started with C programming language!"};
+
+    printf("\n%s\n\n", words);
+
+    return 0;
+}
