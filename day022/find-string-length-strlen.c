4a80351762f9b921b17bb67053a056993e2da9d8
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-09-07 18:30:10 +0100

    chore: finding the string length using strlen()

---
+++ day022/find-string-length-strlen.c
@@ -0,0 +1,12 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char words[] = {"Hey! Let's get started with C programming language!"};
+    
+    printf("Lenght: %d\n", strlen(words));
+
+    return 0;
+}
