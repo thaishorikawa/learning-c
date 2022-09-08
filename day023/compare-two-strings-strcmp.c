192395ae554cc76f3f905af5c8ca8361b7439594
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-09-08 21:36:26 +0100

    chore: compare two strings with strcmp()

---
+++ day023/compare-two-strings-strcmp.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char word1[] = {"information"};
+    char word2[] = {"technology"};
+
+    printf("\nResult: %d\n", strcmp(word1, word2));
+
+    return 0;
+}
