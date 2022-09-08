ed981e5fbf1cd7d0625aeac997711eef5da3c665
Thaís Horikawa Gomes <thaishorikawa@hotmail.com>
2022-09-08 22:53:24 +0100

    chore: finding the first occurrence of a character in a string with strchr()

---
+++ day023/find-char-string-strchr.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char word[20] = {"window"};
+    char *letter;
+
+    letter = strchr(word, 'w');
+
+    printf("\n%c\n", *letter);
+    printf("%c\n", *(letter + 1));
+
+    return 0;
+}
