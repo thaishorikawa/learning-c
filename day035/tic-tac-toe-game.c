b57d0963f9acb30b2b5a41117343b5a73bf9f990
thaishorikawa <thaishorikawa@hotmail.com>
2022-09-20 17:59:07 +0100

    chore: display the table (board) and numbers for the second step of tic-tac-toe game

--- day035/tic-tac-toe-game.c
+++ day035/tic-tac-toe-game.c
@@ -15,14 +15,24 @@ void initializeMatrix()
 
 void display()
 {
+    printf("\n\t 0   1   2\n\n");
     for (r = 0; r < 3; r++)
     {
         for (c = 0; c < 3; c++)
         {
-            printf("%c", game[r][c]);
+            if (c == 0)
+                printf("\t");
+            printf(" %c ", game[r][c]);
+            if (c < 2)
+                printf("|");
+            if (c == 2)
+                printf("  %d", r);
         }
         printf("\n");
+        if (r < 2)
+            printf("\t-----------\n");
     }
+    printf("\n");
 }
 
 int main()
