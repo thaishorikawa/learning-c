87964f6a60f7b6d4c529661ca3249c9855aef51e
thaishorikawa <thaishorikawa@hotmail.com>
2022-09-24 20:35:28 +0100

    chore: array of struct

---
+++ day039/array-of-struct.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
+{
+    int day, month, year;
+} BirthDate;
+
+typedef struct 
+{
+    BirthDate birthDate;
+    int age;
+    char gender;
+    char name[100];
+} Person;
+
+void displayPerson(Person p)
+{
+    printf("\n\tName: %s", p.name);
+    printf("\tAge: %d\n", p.age);
+    printf("\tGender: %c\n", p.gender);
+    printf("\tBirth Date: %d/%d/%d\n\n", p.birthDate.day, p.birthDate.month, p.birthDate.year);
+}
+
+Person readPerson()
+{
+    Person p;
+
+    printf("\nEnter your name: ");
+    fgets(p.name, 100, stdin);
+    printf("Enter your age: ");
+    scanf("%d", &p.age);
+    printf("Enter f or m for gender: ");
+    scanf(" %c", &p.gender);
+    printf("Enter your birth date - dd mm yyyy: ");
+    scanf("%d%d%d", &p.birthDate.day, &p.birthDate.month, &p.birthDate.year);
+    getchar();
+    return p;
+}
+
+int main()
+{
+    int i;
+    Person people[3];
+    
+    for (i = 0; i < 3; i++)
+    {
+        people[i] = readPerson();
+        displayPerson(people[i]);
+    }
+
+    return 0;
+}
