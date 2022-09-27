1c0973a4faaea6becb5733af1a192f0644c58ffe
thaishorikawa <thaishorikawa@hotmail.com>
2022-09-27 16:20:30 +0100

    chore: verify if the coordinates are valid, number of adjacent bombs and count bombs

--- day041/minesweeper.c
+++ day041/minesweeper.c
@@ -6,7 +6,7 @@ typedef struct
 {
     int isBomb;
     int isOpen;
-    int neighbors;
+    int adjacent;
 } Cell;
 
 Cell game[10][10];
@@ -20,7 +20,7 @@ void startGame()
         {
             game[r][c].isBomb = 0;
             game[r][c].isOpen = 0;
-            game[r][c].neighbors = 0;
+            game[r][c].adjacent = 0;
         }
     }
 }
@@ -40,10 +40,43 @@ void drawBombs(int n)
     }
 }
 
+int isValidCoordinates(int r, int c)
+{
+    if (r >= 0 && r < size && c >= 0 && c < size)
+        return 1;
+    else
+        return 0;
+}
+
+int quantAdjacentBombs(int r, int c)
+{
+    int quantity = 0;
+
+    if (isValidCoordinates(r - 1, c) && game[r - 1][c].isBomb)
+        quantity++;
+    if (isValidCoordinates(r + 1, c) && game[r + 1][c].isBomb)
+        quantity++;
+    if (isValidCoordinates(r, c + 1) && game[r][c + 1].isBomb)
+        quantity++;
+    if (isValidCoordinates(r, c - 1) && game[r][c - 1].isBomb)
+        quantity++;
+    return quantity;
+}
+
+void countingBombs()
+{
+    for (r = 0; r < size; r++)
+    {
+        for (c = 0; c < size; c++)
+            game[r][c].adjacent = quantAdjacentBombs(r, c);
+    }
+}
+
 int main()
 {
     startGame();
     drawBombs(10);
+    countingBombs();
 
     return 0;
 }
