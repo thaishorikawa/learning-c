9bcdda4ab5ecfd9a842376f92b32db1baf4d1252
thaishorikawa <thaishorikawa@hotmail.com>
2022-10-08 20:50:43 +0100

    chore: function to register contacts of the birthday calendar

--- day052/birthday-calendar.c
+++ day052/birthday-calendar.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct 
+typedef struct
 {
     char name[50];
     int day, month, year;
@@ -15,9 +15,29 @@ void display(Contact **c, int quant)
     printf("\t--------------------------------\n");
     for (i = 0; i < quant; i++)
     {
-        printf("\t%d = %2d/%2d/%4d\t%s\n", i+1, c[i]->day, c[i]->month, c[i]->year, c[i]->name);
+        printf("\t%d = %2d/%2d/%4d\t%s\n", i + 1, c[i]->day, c[i]->month, c[i]->year, c[i]->name);
     }
     printf("\t--------------------------------\n");
+}
+
+int registerContact(Contact **c, int quant, int size)
+{
+    if (quant < size)
+    {
+        Contact *new = malloc(sizeof(Contact));
+
+        printf("\nContact name: ");
+        scanf("%50[^\n]", new->name);
+        printf("\nContact birthday date dd mm yyyy: ");
+        scanf("%d%d%d", &new->day, &new->month, &new->year);
+        getchar();
+        c[quant] = new;
+        return 1;
+    }
+    else
+    {
+        printf("\n\tIt is not possible to create a new register. Array is full!\n");
+    }
 }
 
 int main()
@@ -25,6 +45,13 @@ int main()
     Contact *calendar[50];
     int size = 50, quant = 0;
 
+    display(calendar, quant);
+    quant += registerContact(calendar, quant, size);
+    printf("\nquant: %d\n", quant);
+
+    display(calendar, quant);
+    quant += registerContact(calendar, quant, size);
+    printf("\nquant: %d\n", quant);
     display(calendar, quant);
 
     return 0;
